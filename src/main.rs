e11e0941faca310a1be748861d4fd3462b48b738
Roy van de Water <roy.v.water@gmail.com>
2023-11-15 07:40:33 -0700

    UDP example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod protos;
 
 use std::{
-    net::{TcpListener, TcpStream},
+    net::UdpSocket,
     thread::{self, JoinHandle},
 };
 
-use protobuf::{CodedInputStream, Message};
-use protos::generated::pingpong::Ping;
+use protobuf::Message;
+use protos::generated::pingpong::{Ping, Pong};
 
-use crate::protos::generated::pingpong::Pong;
+const SERVER_ADDR: &str = "127.0.0.1:3000";
+const CLIENT_ADDR: &str = "127.0.0.1:3001";
 
 fn main() {
     println!("Hello, world!");
@@ -22,28 +23,70 @@ fn main() {
 
 fn start_client() -> JoinHandle<()> {
     println!("C: Starting... ");
-    let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    let socket = UdpSocket::bind(CLIENT_ADDR).unwrap();
     println!("C: Connected.");
 
     thread::spawn(move || {
         let mut index = 0;
-        let mut incoming_stream = stream.try_clone().unwrap();
-        let mut incoming = CodedInputStream::new(&mut incoming_stream);
+        let mut send_buf = Vec::with_capacity(1024);
+        let mut recv_buf = [0; 1024];
 
         loop {
             index += 1;
-
             if index > 100 {
-                println!("C: Disconnecting");
+                socket.send_to(&[0x04], SERVER_ADDR).unwrap();
                 return;
             }
 
             let ping = new_ping(index);
             println!("C: Sending Ping! ({})", ping.index());
-            ping.write_length_delimited_to_writer(&mut stream).unwrap();
 
-            let pong: Pong = incoming.read_message().unwrap();
-            println!("C: Received Pong! ({})", pong.index());
+            ping.write_to_vec(&mut send_buf).unwrap();
+            socket.send_to(&send_buf, SERVER_ADDR).unwrap();
+
+            loop {
+                let num_read = socket.recv(&mut recv_buf).unwrap();
+                if num_read == 0 {
+                    continue;
+                }
+
+                let bytes = &mut recv_buf[..num_read];
+                let pong = Pong::parse_from_bytes(&bytes).unwrap();
+                println!("C: Received Pong! ({})", pong.index());
+                break;
+            }
+        }
+    })
+}
+
+fn start_server() -> JoinHandle<()> {
+    println!("S: Starting... ");
+    let socket = UdpSocket::bind("127.0.0.1:3000").unwrap();
+    println!("S: Started.");
+
+    thread::spawn(move || {
+        let mut buf = [0; 1024];
+
+        loop {
+            let (num_read, addr) = socket.recv_from(&mut buf).unwrap();
+            if num_read == 0 {
+                continue;
+            }
+            let bytes = &mut buf[..num_read];
+            if bytes[0] == 0x04 {
+                println!("S: Received shutdown signal.");
+                return;
+            }
+            // println!("S: Received bytes: {:?}", bytes);
+
+            let ping = Ping::parse_from_bytes(&bytes).unwrap();
+            println!("S: Received Ping! ({})", ping.index());
+
+            let pong = new_pong(ping.index());
+            println!("S: Sending Pong! ({})", pong.index());
+
+            let send_buf = pong.write_to_bytes().unwrap();
+            socket.send_to(&send_buf, &addr).unwrap();
         }
     })
 }
@@ -54,31 +97,8 @@ fn new_ping(index: i32) -> Ping {
     ping
 }
 
-fn start_server() -> JoinHandle<()> {
-    println!("S: Starting... ");
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    println!("S: Started.");
-
-    thread::spawn(move || {
-        for stream_result in listener.incoming() {
-            let mut stream = stream_result.unwrap();
-
-            let mut incoming_stream = stream.try_clone().unwrap();
-            let mut incoming = CodedInputStream::new(&mut incoming_stream);
-
-            loop {
-                if incoming.eof().unwrap() {
-                    println!("S: Client disconnected.");
-                    return;
-                }
-                let ping: Ping = incoming.read_message().unwrap();
-                println!("S: Received Ping! ({})", ping.index());
-
-                let mut pong = Pong::new();
-                pong.set_index(ping.index());
-                println!("S: Sending Pong! ({})", pong.index());
-                pong.write_length_delimited_to_writer(&mut stream).unwrap();
-            }
-        }
-    })
+fn new_pong(index: i32) -> Pong {
+    let mut pong = Pong::new();
+    pong.set_index(index);
+    pong
 }
